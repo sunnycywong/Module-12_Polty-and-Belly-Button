58582e49751e3ae7cd43d1388d4a44744bebbe87
swong328 <65827019+swong328@users.noreply.github.com>
2022-04-30 12:19:33 -0400

    update

---
+++ plot.js
@@ -0,0 +1,65 @@
+// Original - Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+
+// var trace = {
+//     labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+//     "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+//     values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+//     type: 'pie'
+//    };
+//    var data = [trace];
+//    var layout = {
+//     title: "'Pie' Chart",
+//    };
+//    Plotly.newPlot("plotArea", data, layout);
+   
+// var topFiveCityNames = cityGrowths.map(city => city.City);
+// var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
+   
+
+// var trace = {
+//     x: topFiveCityNames,
+//     y: topFiveCityGrowths,
+//     type: "bar"
+//   };
+//   var data = [trace];
+//   var layout = {
+//     title: "Most Rapidly Growing Cities",
+//     xaxis: {title: "City" },
+//     yaxis: {title: "Population Growth, 2016-2017"}
+//   };
+//   Plotly.newPlot("bar-plot", data, layout);
+
+
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+
+init();
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(result.location);
+  });
+}
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
